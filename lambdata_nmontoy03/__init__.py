4d1ae3ddc6c6191d0a1510f614c2a7523b9357d8
Lambda_School_Loaner_95 <lambda_school_loaner_95@Lambda-School-Loaner-95s-MacBook-Pro.local>
2019-05-27 18:42:38 -0700

    Adding initial POC package/module

---
+++ lambdata_nmontoy03/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+lambdata - a collection of Data Science helper functions
+"""
+
+import pandas as pd 
+import numpy as np 
+
+TEST = pd.DataFrame(np.ones(10)) 
+
+def check_nulls(df):
+    """
+    Prints out columns and numbers of nan's it has
+    """
+    print(df.isna().sum())
+
+def split_date(dates):
+    """
+    Function to split dates ("MM/DD/YYYY", etc.) into multiple columns
+    Returns dataframe called 'dates' containing three new columns
+    """
+    split_dates = pd.DataFrame()
+    dates = pd.to_datetime(dates, format='%Y-%m-%d')
+    split_dates['year'] = dates.dt.year
+    split_dates['month'] = dates.dt.month 
+    split_dates['day'] = dates.dt.day
+
+    return split_dates 
+
+
